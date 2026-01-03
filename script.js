bce92628748fcaf60909f3fb6fa1f8fa13b74683
Vaishnavi Chauhan <chauhanvaishnavi554@gmail.com>
2026-01-03 22:54:13 +0530

    Added contact page

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("NGO Website Loaded Successfully");
+const form = document.getElementById("contactForm");
+const successMsg = document.getElementById("successMsg");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let valid = true;
+  successMsg.textContent = "";
+
+  function error(input, msg) {
+    input.parentElement.querySelector("small").textContent = msg;
+    valid = false;
+  }
+  function clear(input) {
+    input.parentElement.querySelector("small").textContent = "";
+  }
+
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const message = document.getElementById("message");
+
+  name.value.length < 3 ? error(name,"Min 3 characters") : clear(name);
+  !/^\S+@\S+\.\S+$/.test(email.value) ? error(email,"Invalid email") : clear(email);
+  phone.value && !/^\d{10}$/.test(phone.value) ? error(phone,"10 digit number") : clear(phone);
+  message.value.length < 10 ? error(message,"Min 10 characters") : clear(message);
+
+  if(valid){
+    successMsg.textContent = "✅ Message sent successfully!";
+    form.reset();
+  }
+});
